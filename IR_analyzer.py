e6cfd51c75a9f26f9aab9fd21c4fc7d5efb9540d
4m1g0 <dev.4m1g0@gmail.com>
2016-07-10 04:41:48 +0200

    Draw graphs showing the spectrum

--- IR_analyzer.py
+++ IR_analyzer.py
@@ -4,6 +4,7 @@ import bisect
 import serial
 from serial.tools import list_ports
 import sys
+import multiprocessing
 
 
 def scatterplot(x,y):
@@ -27,6 +28,26 @@ def main():
     arduino = serial.Serial('/dev/' + ports[port].name, 9600)
     
     while True:
-        print(arduino.readline())
+        burst = arduino.readline()
+        print(burst)
+        print()
+        
+        burst = burst.replace(b'\r', b'').replace(b'\n', b'')
+        y = [abs(int(x)-49) for x in burst]
+        print(y)
+        x = range(0, len(y))
+        Draw(x, y).start()
 
+class Draw(multiprocessing.Process):
+    def __init__(self, x, y):
+        multiprocessing.Process.__init__(self)
+        self.x = x
+        self.y = y
+        
+    def run(self):
+        pyplot.plot(self.x, self.y)
+        pyplot.ylim(-1.5,1.5)
+        pyplot.show()
+        #scatterplot(self.x, self.y)
+        
 main()
